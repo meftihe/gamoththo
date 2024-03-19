ad8b4ac94c96a03d544fc70493fe9ac0097556d3
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-19 14:31:54 +0300

    add dark theme toggle in the template

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,3 +1,28 @@
 <template>
-  <h2>Settings page</h2>
+  <div class="container mx-auto">
+    <h1 class="text-xl font-bold my-4">Settings</h1>
+    <div class="grid grid-cols-1 gap-4">
+      <div class="card shadow-lg">
+        <div class="card-body">
+          <h2 class="card-title">Appearance</h2>
+          <p>Change the appearance of the app</p>
+          <div class="form-control">
+            <label class="label cursor-pointer">
+              <span class="label-text">Dark Theme</span>
+              <input type="checkbox" class="toggle" checked />
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="card shadow-lg">
+        <div class="card-body">
+          <h2 class="card-title">Security</h2>
+          <p>Change your security settings</p>
+          <nuxt-link to="/settings/security" class="btn btn-primary">
+            Security settings
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
