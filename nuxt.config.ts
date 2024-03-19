2f848e994488080a242e218c8b6a71dffd991e7e
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-19 14:27:09 +0300

    install nuxt color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/color-mode'],
   build: {
     transpile: ['vue-echarts', 'resize-detector'],
   },
