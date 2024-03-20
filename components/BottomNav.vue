2fae5e47fdb274605c9b104c9fdefc623d581c06
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-20 15:07:49 +0300

    Create settings page and add theme toggle feature (#32)
    * add active toggle in the bottom nav for active pages
    
    * remove hamburger menu from header
    
    * install nuxt color mode
    
    * add dark theme toggle in the template
    
    * add color mode configuration
    
    * implement theme toggle using colorMode

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="btm-nav">
-    <button class="active">
+  <div class="btm-nav btm-nav-sm">
+    <nuxt-link to="/" :class="$route.path === '/' ? 'active' : ''">
       <icon name="ph:house-duotone" class="w-5 h-5"></icon>
-    </button>
-    <button>
+    </nuxt-link>
+    <nuxt-link
+      to="/favorites"
+      :class="$route.path === '/favorites' ? 'active' : ''"
+    >
       <icon name="ic:twotone-favorite" class="w-5 h-5"></icon>
-    </button>
-    <button>
+    </nuxt-link>
+    <nuxt-link
+      to="/history"
+      :class="$route.path === '/history' ? 'active' : ''"
+    >
       <icon name="iconamoon:history-duotone" class="w-5 h-5"></icon>
-    </button>
-    <button>
-      <icon name="ic:twotone-settings" class="w-5 h-5"></icon>
-    </button>
+    </nuxt-link>
+    <nuxt-link
+      to="/settings"
+      :class="$route.path === '/settings' ? 'active' : ''"
+    >
+      <icon name="ph:gear-duotone" class="w-5 h-5"></icon>
+    </nuxt-link>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/color-mode'],
   build: {
     transpile: ['vue-echarts', 'resize-detector'],
+  },
+  colorMode: {
+    preference: 'light',
+    fallback: 'light',
+    dataValue: 'theme',
+    classSuffix: '',
   },
 });
