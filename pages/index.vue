bf20c4c36afc9ab42c0b4276d054718b83ca29e4
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-20 16:21:23 +0300

    add search field in the hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <Navbar>Gamoththo</Navbar>
-  <main class="p-4">
-    <h2>The homepage</h2>
-  </main>
+  <div class="bg-base-200">
+    <Navbar class="bg-base-200">Gamoththo</Navbar>
+    <div class="w-full flex flex-col items-center pb-4">
+      <p class="my-5 text-xl">The only dictionary you need in Ethiopia.</p>
+      <label
+        class="input input-bordered input-md rounded-full flex items-center gap-2"
+      >
+        <input type="text" class="grow" placeholder="Search" />
+        <icon name="feather:search" class="w-4 h-4 opacity-70" />
+      </label>
+    </div>
+  </div>
+
+  <div></div>
 </template>
