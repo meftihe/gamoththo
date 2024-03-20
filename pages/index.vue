16aa2ac7a85d882385f7d8d767f03d77e78865e5
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-20 16:24:58 +0300

    fix horizontal and vertical padding of the hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="bg-base-200">
     <Navbar class="bg-base-200">Gamoththo</Navbar>
-    <div class="w-full flex flex-col items-center pb-4">
-      <p class="my-5 text-xl">The only dictionary you need in Ethiopia.</p>
+    <div class="w-full flex flex-col items-center px-4 py-8">
+      <p class="my-5 text-xl text-center">
+        The only dictionary you need in <b>Ethiopia</b>.
+      </p>
       <label
         class="input input-bordered input-md rounded-full flex items-center gap-2"
       >
